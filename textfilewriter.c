b4f6943d2fef259b0b40993909387062e0d5ba22
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2022-06-07 16:26:58 +0900

    complete the command - save, load modified linkedlist.c fuction - print_file modified textfilewriter.c fuction - read_file, write_file, create_music_titles

---
+++ textfilewriter.c
@@ -0,0 +1,31 @@
+#include "textfilewriter.h"
+#include "linkedlist.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+void create_music_titles(FILE* stream) {
+    int NUM_OF_MUSIC = 0;
+    static char str[10][MAX_TITLE_SIZE];
+    fscanf(stream, "%d\n", &NUM_OF_MUSIC);
+
+    for (int i = 0; i < NUM_OF_MUSIC; i++) {
+        fscanf(stream, "%s", str[i]);
+        append_left(strlen(str[i]), str[i]);
+    }
+}
+
+
+void read_file(char* file_name) {
+    FILE *fp;
+    fp = fopen(file_name, "r");
+    create_music_titles(fp);
+    fclose(fp);
+}
+void write_file(char* file_name) {
+    FILE *fp;
+    fp = fopen(file_name, "w");
+
+    print_file(fp);
+
+    fclose(fp);
+}
